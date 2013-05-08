4dc0a182cce4d638a10fe86ee0d292326a3ddc75
Jay Jagpal <jay@strangemother.com>
2013-05-08 23:38:19 +0100

    methodify connection strings

--- media/js/pocketsocket.js
+++ media/js/pocketsocket.js
@@ -101,7 +101,7 @@ methods = {
                 } else {
                     console.log('Not Connected', socket)
                 }
-            
+
             } else {
                 throw new AddressException('Socket does not exist', name);
             }
@@ -109,9 +109,9 @@ methods = {
         },
         sendToAll: function(str, val){
             /*
-            send message  to all connected. 
+            send message  to all connected.
             If in broadcast, all open sockets will be used.
-            If the socket (or any broadcast socket) is disconnected, 
+            If the socket (or any broadcast socket) is disconnected,
             reconnect will be initiated and a queued message will be sent later
             */
             var str = arg(arguments, 0, null);
@@ -119,7 +119,7 @@ methods = {
 
             var val = arg(arguments, 1, {});
 
-            
+
             var _sents = []
             for(var socketName in this.__sockets) {
                 var socket = this.__sockets[socketName];
@@ -132,11 +132,11 @@ methods = {
         },
         signal: function(channel, eventName) {
             /*
-            Signal is passed to method to be pushed into the 
+            Signal is passed to method to be pushed into the
             websocket streams. This should be successful for broadcast and
             overflow.
 
-            If the connection does not exist, a new connection will be 
+            If the connection does not exist, a new connection will be
             created based on connection setup.
 
              */
@@ -153,26 +153,26 @@ methods = {
 
             // if val is function, special hook to use with ID.
 
-            if(socketName == null){ 
+            if(socketName == null){
                 // send on every available socket.
-                 
+
                 // check socket exist.
-                
+
                 if(this.connection.connectionType == 'broadcast') {
-                    // Connect to every dead connection using the 
+                    // Connect to every dead connection using the
                     // reconnect method on a socket.
                     // send all.
                     this.sendToAll(sendStr, val || {});
                 }
                 // if not connected, connect.
-                
+
             } else {
                 // if socket name exists/
                 // send
                 // if dead:
                 //  reconnect, send later.
                 this.sendTo(socketName, sendStr, val || {})
-            } 
+            }
 
             /*
             var c = this.connect('ws://127.0.0.1:8001', function(name, ev){
@@ -197,17 +197,17 @@ methods = {
         },
         on: function(){
             // two methods to call
-            
+
             // on('socket', function(data, event))
             //  // Channel in data object.
-            
+
             // on('socket', 'name', function(data, event))
             //  // Data is the object sent from the signal.
 
             var channel  = arg(arguments, 0, null);
             var cf1      = arg(arguments, 1, null);
             var cf2      = arg(arguments, 2, null);
-        
+
             var callfunc = cf2;
             var socket   = this.__socket;
             var _d = methods.webSocket.__data;
@@ -227,11 +227,11 @@ methods = {
                         cf2(e, data);
                     }
                 }
-                
+
             })
-            
+
             /*
-            
+
             if(!socket) {
                 // store hooks for later.
                 if(!_d.hasOwnProperty('prehooks') ) {  _d.prehooks = []; };
@@ -242,7 +242,7 @@ methods = {
             socket.eventHook(channel, function(name, ev) {
                 if(cf2 == null && cf2 == name) {
                     socket.eventHook(channel, ev);
-                    
+
                     // Call single  message  hook.
                     // cf1(ev, ev.__event);
                 } else {
@@ -253,23 +253,21 @@ methods = {
 
             return this;
         },
-        setup: function(){
+        convertForConnect: function() {
+            /*
+            Pass data object to be parsed as connection data.
+            Returned will be a pocket.socket ready object of connection.
+            The returned Array can be used for setup() and connect()
+             */
             var address = arg(arguments, 0, '127.0.0.1');
             var ports   = arg(arguments, 1, null);
-            var listType = arg(arguments, 2, null);
-            var conType = arg(arguments, 3, null);
-
-            // setup('127.0.0.1:8001')
-            // setup('127.0.0.1:8001', 'broadcast')
-            this.connection.empty()
-            // sort other object.
-            
             var a, p;
+
             if(address instanceof Array) {
                 // List of ips, should have list of ports
                 // [ '127.0.0.1', '127.0.0.2' ]
                 a = address
-      
+
 
             } else if(typeof(address) == 'string') {
 
@@ -280,7 +278,7 @@ methods = {
                     a = [address];
                 }
             } else if(typeof(address) == 'object') {
-                var _a = [], 
+                var _a = [],
                     _p = [];
 
                 for(var name in address) {
@@ -312,12 +310,32 @@ methods = {
                 p = [ports];
             }
 
-            //debugger;
+            returnObject = {
+                'addresses': a,
+                'ports': p
+            }
+
+            return returnObject;
+        },
+        setup: function(){
+            var address = arg(arguments, 0, '127.0.0.1');
+            var ports   = arg(arguments, 1, null);
+            var listType = arg(arguments, 2, null);
+            var conType = arg(arguments, 3, null);
+
+            // setup('127.0.0.1:8001')
+            // setup('127.0.0.1:8001', 'broadcast')
+            this.connection.empty()
+            // sort other object.
+
+            var connectionObject = this.convertForConnect(address, ports)
+            var a = connectionObject.addresses;
+            var p = connectionObject.ports;
 
             if(conType == null) {
                 conType = 'broadcast';
             }
-            
+
             this.connection.addresses = a;
             this.connection.ports = p;
             this.connection.connectionType = conType;
@@ -338,7 +356,7 @@ methods = {
             // Receive the next address from a created list.
             isSetup: function(){
                 /* Has the library been setup and has at least one connection */
-                if(this.addresses 
+                if(this.addresses
                     && (this.addresses instanceof Array)
                     && this.addresses.length > 0) {
                         return true;
@@ -347,14 +365,14 @@ methods = {
                 return false;
             },
             addToList: function(list, ip, port, name) {
-                // Crate a connection element to be enumerated and 
+                // Crate a connection element to be enumerated and
                 // used for connections
                 var o = {};
                 if(ip && port) {
                     o.ip = ip;
                     o.port = port;
                     o.name = name || '';
-                    
+
                     o.toString = function(){
                         return this.ip + ':' + this.port;
                     }
@@ -368,9 +386,9 @@ methods = {
                 /* Build the complete list of all connection types.
                 As this is never exists. - Instead, the index counter
                 used to provide the next permutation of the next() counter
-                is reset and the lists are enumerated until all 
+                is reset and the lists are enumerated until all
                 possible outcomes are created.
-                If listType is  'mix' 
+                If listType is  'mix'
                                 'flat'
                 Returned is an Array of objects
                 [
@@ -379,7 +397,7 @@ methods = {
                         port: 8001
                     }
                 ]
-                */ 
+                */
                 var _list =  [];
                 for (var i = 0; i < this.addresses.length; i++) {
                     var a = this.addresses[i];
@@ -409,7 +427,7 @@ methods = {
 
                             throw new AddressException(s, d);
                         }
-                    } 
+                    }
                 };
                 _list.toArray = function(){
                     // Tidy output for flat view :)
@@ -422,7 +440,7 @@ methods = {
                 }
 
                 return _list;
-            }, 
+            },
 
             getNextOrFirst: function(list, index) {
                 var l = (list instanceof Function)? list.call(this, index): list[0];
@@ -441,21 +459,21 @@ methods = {
                 }
             },
             getPort: function(index) {
-                /* Get a connection based on index or key/value pair key 
+                /* Get a connection based on index or key/value pair key
                 */
                 if (this.ports[index])
                     return this.ports[index];
                 return null;
             },
             getIp: function(index) {
-                /* Get a connection based on index or key/value pair key 
+                /* Get a connection based on index or key/value pair key
                 */
                 if (this.addresses && this.addresses[index])
                     return this.addresses[index];
                 return null;
             },
             empty: function() {
-                /* Remove all ip's and port's. 
+                /* Remove all ip's and port's.
                 Does not close the live connection. */
                 this.ips = [];
                 this.ports = [];
@@ -474,9 +492,10 @@ methods = {
             Data passed will be implemented through the connection.setup()
             method.
             */
+           debugger;
             var connections = arg(arguments, 0, null);
-            
-            if(typeof(connections) == 'string' && 
+
+            if(typeof(connections) == 'string' &&
                 arg(arguments,1, null) instanceof Function) {
 
             }
@@ -485,28 +504,28 @@ methods = {
 
             // Broadcast or overflow
             var connectionType = arg(arguments, 3, (this.connection)? this.connection.connectionType: this.connectionType);
-             // Connect to every 
+             // Connect to every
             if(!connections) {
                 connections = (this.connection)? this.connection.list(listType): this.list(listType)
             } else {
                 throw new AddressException('Missing connections')
                 // connections = this.setup(connections, listType, connectionType, false)
             }
-            
+
             // check extra undefined connection
-            //  
+            //
             if(connectionType == 'broadcast') {
                 // Connect to all.
                 var open = [],
                     connected = [];
-                
+
                 // Create connections.
                 connections.forEach(function(e,i,a){
                     // debugger;
                     pocket.socket.makeSocket(e, function(name, data){
                         if(name == 'open') {
-                            open.push(data); 
-                            if(open.length == 2) { 
+                            open.push(data);
+                            if(open.length == 2) {
                                 pocket.socket.signal('socket', 'broadcast-all-connected', data.name)
                             }
                         }
@@ -515,7 +534,7 @@ methods = {
             }
         },
         makeSocket: function(){
-            
+
             var u    = arguments[0];                 // url
             var c    = arguments[1] || function(){}; // callback
             var self = this;
@@ -529,7 +548,7 @@ methods = {
                         break;
                     case 'close':
                         //console.log("iSocket closed");
-                        self.start_connect_timer(self.__sockets[u]) 
+                        self.start_connect_timer(self.__sockets[u])
                         break;
                     case 'message':
                         //console.log("iSocket message", data);
@@ -601,12 +620,12 @@ methods = {
 
             if(typeof(_url) == 'string') {
                 var sli = _url.slice(0, 3);
-                if(sli != 'ws:' && sli != 'wss') { 
+                if(sli != 'ws:' && sli != 'wss') {
                     if(secure) {
                         return 'wss://' + _url;
                     }
-                    return 'ws://' + _url;   
-                } 
+                    return 'ws://' + _url;
+                }
             } else {
                 console.warn("Error with url", url, _url)
             }
@@ -617,22 +636,22 @@ methods = {
             // new AugementedWebSocket(url)
             var uri = arg(arguments, 0, null);
             var socket = new WebSocket(uri)
-            
+
             var reconnect = function reconnect() {
                 socket.close()
-               
+
                 pocket.socket.websocket.close(socket)
                 // reapply reconnect function
                 socket = this.connect(uri, function(){
                     console.log("Socket reconnected", socket)
                 })
 
-            } 
+            }
 
             var sendJson = function sendJson(message, data){
                 return pocket.webSocket.sendJson(socket, message, data);
             }
-            
+
             socket.uri = uri
             socket.connected = false;
             socket.sendJson = sendJson;
@@ -655,10 +674,10 @@ methods = {
             }
 
             var _uri = this.socketString(uri);
- 
+
             // Send out a connecting signal
             this.__callHook('socket', 'connecting', [uri, _uri]);
-            
+
             // ws-URI = "ws:" "//" host [ ":" port ] path [ "?" query ]
             // wss-URI = "wss:" "//" host [ ":" port ] path [ "?" query ]
             var w  = new this.AugmentedWebSocket(_uri);
@@ -667,14 +686,14 @@ methods = {
             w.onopen = function(e) {
                 success(e)
                 // From AugmentedWebSocket
-                w.connected=true; 
+                w.connected=true;
                 self.__callHook('socket', 'open', {
                     socket: w,
                     data: e,
                 })
             };
             w.onclose = function(evt) {
-                w.connected=false; 
+                w.connected=false;
                 self.__callHook('socket', 'close', evt)
             };
             w.onmessage = function(ev) {
@@ -685,7 +704,7 @@ methods = {
                 } catch(e) {
                     json = false;
                 }
-    
+
                 if(!json) {
                     self.__callHook('socket', 'message', ev)
                     return
@@ -703,7 +722,7 @@ methods = {
                     var channel = json.message.split('.')[0];
                     var eventName = json.message.split('.')[1];
                     var data = json.data;
-                    
+
                     json.__channel = channel;
                     json.__eventName = eventName;
                     json.__event = ev;
@@ -726,7 +745,7 @@ methods = {
         sendJson: function(){
             var socket = arg(arguments, 0, null)
             var message = arg(arguments, 1, null)
-            var data = arg(arguments, 2, {})            
+            var data = arg(arguments, 2, {})
             // send a message
             // returns the unique id of the message
             var o = {
@@ -736,8 +755,8 @@ methods = {
             };
 
             var json = JSON.stringify(o);
-            
-            if(socket) {   
+
+            if(socket) {
                 socket.send(json);
                 return o.id;
             } else {
@@ -760,7 +779,7 @@ methods = {
             // call all function hooking channel.
             // eventName and eventData are passed to the called function.
             if(this.__data.hooks.hasOwnProperty(channel)) {
-               
+
                 for(var hookMethod in this.__data.hooks[channel]) {
                     var func = this.__data.hooks[channel][hookMethod];
                     if(func) {
@@ -799,7 +818,7 @@ methods = {
             // add func to name stack object caller
             if(!(name instanceof Array) ) {
                 name = [name]
-            } 
+            }
 
             for (var i = 0; i < name.length; i++) {
                 var _name = name[i]
