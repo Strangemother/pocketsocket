034821f6716b17a60fed3450110c4bdc12068be5
Jay Jagpal <jay@strangemother.com>
2016-12-11 20:53:41 +0000

    Hacked Acceptance for Websocket
    Works - needs cleanup

--- v2/src/server.py
+++ v2/src/server.py
@@ -3,6 +3,7 @@ import socket
 from select import select
 from collections import deque
 import time
+import struct
 import sys
 
 
@@ -20,6 +21,8 @@ else:
     from StringIO import StringIO
 
 
+_VALID_STATUS_CODES = [1000, 1001, 1002, 1003, 1007, 1008,
+                       1009, 1010, 1011, 3000, 3999, 4000, 4999]
 
 class OPTION_CODES:
     '''
@@ -70,6 +73,9 @@ class SocketCreateMixin(object):
         ports = (port,) if port is not None else self.ports
 
         listeners = self.bind_pairs(hosts, ports)
+
+        self._socket = listeners[0]
+
         return listeners
 
     def bind_pairs(self, hosts, ports):
@@ -104,7 +110,7 @@ class HTTPRequest(BaseHTTPRequestHandler):
 
 import hashlib
 import base64
-
+import codecs
 
 HANDSHAKE_STR = (
     "HTTP/1.1 101 Switching Protocols\r\n"
@@ -119,13 +125,19 @@ class SocketClient(object):
     A client for the Connections
     '''
 
-    connected = False
-
     def __init__(self, sock, address):
         self.socket = sock
         self.address = address
         self.headerbuffer = bytearray()
         self.sendq = deque()
+        self.connected = False
+
+        self.state = HEADERB1
+        self.frag_start = False
+        self.frag_type = OPTION_CODES.BINARY
+        self.frag_buffer = None
+        self.frag_decoder = codecs.getincrementaldecoder(
+            'utf-8')(errors='strict')
 
     def handshake(self):
 
@@ -169,6 +181,288 @@ class SocketClient(object):
         self.connected = True
 
         return self.socket, self.address
+
+    def _handleData(self):
+        # do the HTTP header and handshake
+        if self.connected is False:
+            self.handshake()
+        # else do normal data
+        else:
+            data = self.socket.recv(8192)
+            if not data:
+                self.handleError("remote socket closed")
+
+            if VER >= 3:
+                for d in data:
+                    self._parseMessage(d)
+            else:
+                for d in data:
+                    self._parseMessage(ord(d))
+
+    def _parseMessage(self, byte):
+        # read in the header
+        if self.state == HEADERB1:
+
+            self.fin = byte & 0x80
+            self.opcode = byte & 0x0F
+            self.state = HEADERB2
+
+            self.index = 0
+            self.length = 0
+            self.lengtharray = bytearray()
+            self.data = bytearray()
+
+            rsv = byte & 0x70
+            if rsv != 0:
+                self.handleError('RSV bit must be 0')
+
+        elif self.state == HEADERB2:
+            mask = byte & 0x80
+            length = byte & 0x7F
+
+            if self.opcode == OPTION_CODES.PING and length > 125:
+                self.handleError('ping packet is too large')
+
+            if mask == 128:
+                self.hasmask = True
+            else:
+                self.hasmask = False
+
+            if length <= 125:
+                self.length = length
+
+                # if we have a mask we must read it
+                if self.hasmask is True:
+                    self.maskarray = bytearray()
+                    self.state = MASK
+                else:
+                    # if there is no mask and no payload we are done
+                    if self.length <= 0:
+                        try:
+                            self._handlePacket()
+                        finally:
+                            self.state = self.HEADERB1
+                            self.data = bytearray()
+
+                    # we have no mask and some payload
+                    else:
+                        #self.index = 0
+                        self.data = bytearray()
+                        self.state = PAYLOAD
+
+            elif length == 126:
+                self.lengtharray = bytearray()
+                self.state = LENGTHSHORT
+
+            elif length == 127:
+                self.lengtharray = bytearray()
+                self.state = LENGTHLONG
+
+        elif self.state == LENGTHSHORT:
+            self.lengtharray.append(byte)
+
+            if len(self.lengtharray) > 2:
+                self.handleError('short length exceeded allowable size')
+
+            if len(self.lengtharray) == 2:
+                self.length = struct.unpack_from('!H', self.lengtharray)[0]
+
+                if self.hasmask is True:
+                    self.maskarray = bytearray()
+                    self.state = MASK
+                else:
+                    # if there is no mask and no payload we are done
+                    if self.length <= 0:
+                        try:
+                            self._handlePacket()
+                        finally:
+                            self.state = HEADERB1
+                            self.data = bytearray()
+
+                    # we have no mask and some payload
+                    else:
+                        #self.index = 0
+                        self.data = bytearray()
+                        self.state = PAYLOAD
+
+        elif self.state == LENGTHLONG:
+
+            self.lengtharray.append(byte)
+
+            if len(self.lengtharray) > 8:
+                self.handleError('long length exceeded allowable size')
+
+            if len(self.lengtharray) == 8:
+                self.length = struct.unpack_from('!Q', self.lengtharray)[0]
+
+                if self.hasmask is True:
+                    self.maskarray = bytearray()
+                    self.state = MASK
+                else:
+                    # if there is no mask and no payload we are done
+                    if self.length <= 0:
+                        try:
+                            self._handlePacket()
+                        finally:
+                            self.state = HEADERB1
+                            self.data = bytearray()
+
+                    # we have no mask and some payload
+                    else:
+                        #self.index = 0
+                        self.data = bytearray()
+                        self.state = PAYLOAD
+
+        # MASK STATE
+        elif self.state == MASK:
+            self.maskarray.append(byte)
+
+            if len(self.maskarray) > 4:
+                self.handleError('mask exceeded allowable size')
+
+            if len(self.maskarray) == 4:
+                # if there is no mask and no payload we are done
+                if self.length <= 0:
+                    try:
+                        self._handlePacket()
+                    finally:
+                        self.state = HEADERB1
+                        self.data = bytearray()
+
+                # we have no mask and some payload
+                else:
+                    #self.index = 0
+                    self.data = bytearray()
+                    self.state = PAYLOAD
+
+        # PAYLOAD STATE
+        elif self.state == PAYLOAD:
+            if self.hasmask is True:
+                self.data.append(byte ^ self.maskarray[self.index % 4])
+            else:
+                self.data.append(byte)
+
+            # if length exceeds allowable size then we except and remove the
+            # connection
+            if len(self.data) >=MAXPAYLOAD:
+                self.handleError('payload exceeded allowable size')
+
+            # check if we have processed length bytes; if so we are done
+            if (self.index+1) == self.length:
+                try:
+                    self._handlePacket()
+                finally:
+                    #self.index = 0
+                    self.state = HEADERB1
+                    self.data = bytearray()
+            else:
+                self.index += 1
+
+    def _handlePacket(self):
+        if self.opcode == OPTION_CODES.CLOSE:
+            pass
+        elif self.opcode == OPTION_CODES.STREAM:
+            pass
+        elif self.opcode == OPTION_CODES.TEXT:
+            print 'DATA:', self.data
+        elif self.opcode == OPTION_CODES.BINARY:
+            pass
+        elif self.opcode == OPTION_CODES.PONG or self.opcode == OPTION_CODES.PING:
+            if len(self.data) > 125:
+                self.handleError('control frame length can not be > 125')
+        else:
+            # unknown or reserved opcode so just close
+            self.handleError('unknown opcode')
+
+        if self.opcode == OPTION_CODES.CLOSE:
+            status = 1000
+            reason = u''
+            length = len(self.data)
+
+            if length == 0:
+                pass
+            elif length >= 2:
+                status = struct.unpack_from('!H', self.data[:2])[0]
+                reason = self.data[2:]
+
+                if status not in _VALID_STATUS_CODES:
+                    status = 1002
+
+                if len(reason) > 0:
+                    try:
+                        reason = reason.decode('utf8', errors='strict')
+                    except:
+                        status = 1002
+            else:
+                status = 1002
+
+            self.close(status, reason)
+            return
+
+        elif self.fin == 0:
+            if self.opcode != OPTION_CODES.STREAM:
+                if self.opcode == OPTION_CODES.PING or self.opcode == OPTION_CODES.PONG:
+                    self.handleError('control messages can not be fragmented')
+
+                self.frag_type = self.opcode
+                self.frag_start = True
+                self.frag_decoder.reset()
+
+                if self.frag_type == OPTION_CODES.TEXT:
+                    self.frag_buffer = []
+                    utf_str = self.frag_decoder.decode(self.data, final=False)
+                    if utf_str:
+                        self.frag_buffer.append(utf_str)
+                else:
+                    self.frag_buffer = bytearray()
+                    self.frag_buffer.extend(self.data)
+
+            else:
+                if self.frag_start is False:
+                    self.handleError('fragmentation protocol error')
+
+                if self.frag_type == OPTION_CODES.TEXT:
+                    utf_str = self.frag_decoder.decode(self.data, final=False)
+                    if utf_str:
+                        self.frag_buffer.append(utf_str)
+                else:
+                    self.frag_buffer.extend(self.data)
+
+        else:
+            if self.opcode == OPTION_CODES.STREAM:
+                if self.frag_start is False:
+                    self.handleError('fragmentation protocol error')
+
+                if self.frag_type == OPTION_CODES.TEXT:
+                    utf_str = self.frag_decoder.decode(self.data, final=True)
+                    self.frag_buffer.append(utf_str)
+                    self.data = u''.join(self.frag_buffer)
+                else:
+                    self.frag_buffer.extend(self.data)
+                    self.data = self.frag_buffer
+
+                self.handleMessage()
+
+                self.frag_decoder.reset()
+                self.frag_type = OPTION_CODES.BINARY
+                self.frag_start = False
+                self.frag_buffer = None
+
+            elif self.opcode == OPTION_CODES.PING:
+                self._sendMessage(False, OPTION_CODES.PONG, self.data)
+
+            elif self.opcode == OPTION_CODES.PONG:
+                pass
+
+            else:
+                if self.frag_start is True:
+                    self.handleError('fragmentation protocol error')
+
+                if self.opcode == OPTION_CODES.TEXT:
+                    try:
+                        self.data = self.data.decode('utf8', errors='strict')
+                    except Exception as exp:
+                        self.handleError('invalid utf-8 payload', exp)
 
     def _sendBuffer(self, buff):
         size = len(buff)
@@ -267,7 +561,7 @@ class ConnectionIteratorMixin(object):
             # print 'write_list', ready, len(client.sendq), client
 
             while client.sendq:
-                print ' Sending payload'
+                print 'write_list Sending payload:', client
                 opcode, payload = client.sendq.popleft()
                 remaining = client._sendBuffer(payload)
                 if remaining is not None:
@@ -325,8 +619,8 @@ class ConnectionIteratorMixin(object):
         '''
         Determine if the given client is a server socket for clients.
         '''
-        for fileno in self.listeners:
-            if fileno == client:
+        if client == self._socket:
+            # if fileno == client:
                 return True
         return False
 
