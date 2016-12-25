15a19d3506e7df493aea212198b37f1d2d1db4f3
Jay Jagpal <jay@strangemother.com>
2016-12-25 20:01:02 +0000

    Inherited logger correctly

--- v2/pocketsocket/logger.py
+++ v2/pocketsocket/logger.py
@@ -4,9 +4,24 @@ from functools import partial
 
 class Log(logging.getLoggerClass()):
 
-    def info(self, *args, **kw):
+    def _fm(self,  *args, **kw):
         msg = ' '.join([str(x) for x in args])
-        super(Log, self).info(msg, **kw)
+        return msg
+
+    def info(self, *args, **kw):
+        super(Log, self).info(self._fm(*args, **kw), **kw)
+
+    def debug(self, *args, **kw):
+        super(Log, self).debug(self._fm(*args, **kw), **kw)
+
+    def warning(self, *args, **kw):
+        super(Log, self).warning(self._fm(*args, **kw), **kw)
+
+    def error(self, *args, **kw):
+        super(Log, self).error(self._fm(*args, **kw), **kw)
+
+    def critical(self, *args, **kw):
+        super(Log, self).critical(self._fm(*args, **kw), **kw)
 
 logging.setLoggerClass(Log)
 logging.basicConfig(format='%(funcName)s :: %(message)s')
@@ -18,12 +33,8 @@ logger.setLevel(logging.DEBUG)
 def log_type(l_type, *args):
     return getattr(logger, l_type)(' '.join([str(x) for x in args]))
 
-
-# log = partial(log_type, 'info')
 log = logger.info
 logd = logger.debug
 logw = logger.warning
 loge = logger.error
 logc = logger.critical
-
-log('-- logger created --')
