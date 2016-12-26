feb2c863a3c8589f5d63bad4fe1f3bc8769b8028
Jay Jagpal <jay@strangemother.com>
2016-12-26 23:01:03 +0000

    new client tests

--- v2/pocketsocket/tests/test_client.py
+++ v2/pocketsocket/tests/test_client.py
@@ -1,120 +1,157 @@
 import unittest
+from mock import MagicMock
+import socket
+from client import Listener, SocketClient, PayloadMixin
+from collections import deque
+from states import States, StateHandler, StateManager, OPTION_CODE, STATE, _VALID_STATUS_CODES
 
-'''
-class TestHTTPRequest(unittest.TestCase):
-    def test___init__(self):
-        # h_ttp_request = HTTPRequest(request_text)
-        assert False # TODO: implement your test here
 
-class TestBufferMixin(unittest.TestCase):
-    def test_add_buffer(self):
-        # buffer_mixin = BufferMixin()
-        # self.assertEqual(expected, buffer_mixin.add_buffer(opcode, data, top))
-        assert False # TODO: implement your test here
+class TestListener(unittest.TestCase):
 
-    def test_has_data(self):
-        # buffer_mixin = BufferMixin()
-        # self.assertEqual(expected, buffer_mixin.has_data())
-        assert False # TODO: implement your test here
+    def setUp(self):
+        self.socket = None
 
-    def test_loop_buffer(self):
-        # buffer_mixin = BufferMixin()
-        # self.assertEqual(expected, buffer_mixin.loop_buffer())
-        assert False # TODO: implement your test here
+    def tearDown(self):
 
-    def test_next_buffer(self):
-        # buffer_mixin = BufferMixin()
-        # self.assertEqual(expected, buffer_mixin.next_buffer())
-        assert False # TODO: implement your test here
+        if self.socket is not None:
+            self.socket.close()
 
-class TestServerIntegrationMixin(unittest.TestCase):
-    def test_accept(self):
-        # server_integration_mixin = ServerIntegrationMixin()
-        # self.assertEqual(expected, server_integration_mixin.accept(socket))
-        assert False # TODO: implement your test here
+    def test___repr__(self):
+        host = '127.0.0.1'
+        port = 9100
 
-    def test_close(self):
-        # server_integration_mixin = ServerIntegrationMixin()
-        # self.assertEqual(expected, server_integration_mixin.close(status, reason))
-        assert False # TODO: implement your test here
+        SocketClass = Listener
+        s = SocketClass(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket = s
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        expected = "<Listener(socket.socket): sock unknown>"
+        self.assertEqual(expected, s.__repr__())
+        s.bind((host, port))
+        expected = "<Listener(socket.socket): sock ('127.0.0.1', 9100)>"
+        self.assertEqual(expected, s.__repr__())
 
-    def test_getsockname(self):
-        # server_integration_mixin = ServerIntegrationMixin()
-        # self.assertEqual(expected, server_integration_mixin.getsockname())
-        assert False # TODO: implement your test here
+class TestPayloadMixin(unittest.TestCase):
 
-    def test_handshake(self):
-        # server_integration_mixin = ServerIntegrationMixin()
-        # self.assertEqual(expected, server_integration_mixin.handshake())
-        assert False # TODO: implement your test here
+    def test_encode_payload_data(self):
+        payload_mixin = PayloadMixin()
+        expected = 'test string'
+        data = 'test string'
+        opcode = None
+        fin = None
+        self.assertEqual(expected, payload_mixin.encode_payload_data(data, opcode, fin))
 
-    def test_handshake_response(self):
-        # server_integration_mixin = ServerIntegrationMixin()
-        # self.assertEqual(expected, server_integration_mixin.handshake_response(headers))
-        assert False # TODO: implement your test here
 
-    def test_set_id(self):
-        # server_integration_mixin = ServerIntegrationMixin()
-        # self.assertEqual(expected, server_integration_mixin.set_id(value))
-        assert False # TODO: implement your test here
-
-    def test_start(self):
-        # server_integration_mixin = ServerIntegrationMixin()
-        # self.assertEqual(expected, server_integration_mixin.start(server, listeners, connections))
-        assert False # TODO: implement your test here
-
-class TestWebsocketBinaryPayloadMixin(unittest.TestCase):
-    def test_create_websocket_payload(self):
-        # websocket_binary_payload_mixin = WebsocketBinaryPayloadMixin()
-        # self.assertEqual(expected, websocket_binary_payload_mixin.create_websocket_payload(data, opcode, fin))
-        assert False # TODO: implement your test here
-
-class TestStateHandler(unittest.TestCase):
-    def test_call_state(self):
-        # state_handler = StateHandler()
-        # self.assertEqual(expected, state_handler.call_state(*args, **kw))
-        assert False # TODO: implement your test here
-
-    def test_change_state(self):
-        # state_handler = StateHandler()
-        # self.assertEqual(expected, state_handler.change_state(value))
-        assert False # TODO: implement your test here
-
-    def test_get_current_state(self):
-        # state_handler = StateHandler()
-        # self.assertEqual(expected, state_handler.get_current_state())
-        assert False # TODO: implement your test here
-
-    def test_set_current_state(self):
-        # state_handler = StateHandler()
-        # self.assertEqual(expected, state_handler.set_current_state(v))
-        assert False # TODO: implement your test here
+    def test_handle_payload(self):
+        # payload_mixin = PayloadMixin()
+        # self.assertEqual(expected, payload_mixin.handle_payload())
+        pass
 
 class TestSocketClient(unittest.TestCase):
     def test___init__(self):
-        # socket_client = SocketClient()
-        assert False # TODO: implement your test here
+        socket_client = SocketClient()
+        self.assertIsNone(socket_client.address)
+        self.assertIsInstance(socket_client.buffer_queue, deque)
+        self.assertIsInstance(socket_client._state_manager, StateManager)
+        self.assertIsInstance(socket_client._opcode_manager, StateManager)
 
     def test___repr__(self):
-        # socket_client = SocketClient()
-        # self.assertEqual(expected, socket_client.__repr__())
-        assert False # TODO: implement your test here
+        socket_client = SocketClient()
+        socket_client.address = 'Apples'
+        expected = '<SocketClient "Apples">'
+        self.assertEqual(expected, socket_client.__repr__())
+
 
     def test___unicode__(self):
-        # socket_client = SocketClient()
-        # self.assertEqual(expected, socket_client.__unicode__())
-        assert False # TODO: implement your test here
+        socket_client = SocketClient()
+        socket_client.address = 'Apples'
+        self.assertEqual('Client: Apples', socket_client.__unicode__())
+
+    def test_append_decode_text(self):
+        socket_client = SocketClient()
+        expected = 'Test'
+        self.assertEqual(expected, socket_client.append_decode_text('Test'))
+        socket_client.append_decode_text('three')
+        socket_client.append_decode_text('blind')
+        socket_client.append_decode_text('mice')
+        expected = ['Test', 'three', 'blind', 'mice']
+        self.assertListEqual(expected, socket_client.frag_buffer)
+
+
+    def test_decode_text_fragment(self):
+        socket_client = SocketClient()
+        expected = u'apple"ham#spamDUfUw'
+        data = 'apple\x22ham\x23spam\x44\x55\x66\x55\x77'
+        self.assertEqual(expected, socket_client.decode_text_fragment(data))
+
+    def test_default_manager_caller(self):
+        sc = SocketClient()
+        sc.cake_state = MagicMock(return_value='cherry')
+        manager = sc._state_manager
+        manager._state = 'CAKE'
+        v = sc.default_manager_caller(manager, 1, 2, foo='bar')
+        sc.cake_state.assert_called_with(1,2, foo='bar')
+        self.assertEqual('cherry', v)
+
+    def test_frag_error_if(self):
+        sc = SocketClient()
+        sc.frag_start
+        sc.handleError = MagicMock()
+        sc.frag_error_if('foo')
+        self.assertFalse(sc.handleError.called)
+
+        sc.frag_error_if(False)
+        self.assertEqual(sc.handleError.call_count, 1)
 
     def test_handleError(self):
         # socket_client = SocketClient()
         # self.assertEqual(expected, socket_client.handleError(msg, exc, client))
-        assert False # TODO: implement your test here
+        pass
+
+    def test_handle_byte_chunk(self):
+        socket_client = SocketClient()
+        socket_client._handle_byte = MagicMock()
+        data = 'Test String'
+        socket_client.handle_byte_chunk(data)
+        l = socket_client._handle_byte.call_count
+        self.assertEqual(l, len(data))
 
     def test_sendMessage(self):
-        # socket_client = SocketClient()
-        # self.assertEqual(expected, socket_client.sendMessage(data))
+        socket_client = SocketClient()
+        socket_client._sendMessage = MagicMock()
+        data = 'Example string'
+        socket_client.sendMessage(data, 'opcode')
+        socket_client._sendMessage.assert_called_with(False, 'opcode', data)
+
+class MockSocket(object):
+
+    def getsockname(self):
+        return 'HOST', 'PORT'
+
+'''
+class TestClientListMixin(unittest.TestCase):
+    def test_accept_socket(self):
+        client_list_mixin = ClientListMixin()
+        a = client_list_mixin.accept_socket(MockSocket(), None, None)
+        self.assertEqual(expected, a)
+    def test_client_close(self):
+        # client_list_mixin = ClientListMixin()
+        # self.assertEqual(expected, client_list_mixin.client_close(client, listeners, connections))
         assert False # TODO: implement your test here
 
+    def test_send_all(self):
+        # client_list_mixin = ClientListMixin()
+        # self.assertEqual(expected, client_list_mixin.send_all(data, opcode, ignore))
+        assert False # TODO: implement your test here
+
+    def test_setup(self):
+        # client_list_mixin = ClientListMixin()
+        # self.assertEqual(expected, client_list_mixin.setup(*args, **kw))
+        assert False # TODO: implement your test here
+
+    def test_socket_bind(self):
+        # client_list_mixin = ClientListMixin()
+        # self.assertEqual(expected, client_list_mixin.socket_bind(host, port, socket_class, **kw))
+        assert False # TODO: implement your test here
+'''
 if __name__ == '__main__':
     unittest.main()
-'''
