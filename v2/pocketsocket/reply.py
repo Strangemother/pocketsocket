5651b8e0af624de448072a3b9d6378bc28ecc28b
Jay Jagpal <jay@strangemother.com>
2016-12-26 15:27:43 +0000

    Simple reply example

---
+++ v2/pocketsocket/reply.py
@@ -0,0 +1,16 @@
+from ws import Client
+from server import Server
+
+
+def main():
+    server = Server(client_class=ReplyClient)
+    server.start()
+
+
+class ReplyClient(Client):
+
+    def recv(self, data, opcode):
+        self.send('Thank you.')
+
+if __name__ == '__main__':
+    main()
