a17995099974a032ed6672b0b84f7882ddc1ffaf
Jay Jagpal <jay@strangemother.com>
2017-10-02 18:42:58 +0100

    Quick and dirty ui

---
+++ pocketsocket/web/assets/js/main.js
@@ -0,0 +1,72 @@
+var cleanData = []
+
+
+var jsonFetchApp = new Vue({
+    el: '#main'
+    , data: {
+        address: 'ws://192.168.0.10:9001'
+        , basePath: ''
+        , requests: []
+        , selected: {}
+        , message: undefined
+        , socketMessages: []
+        , connected: false
+        , indexItem: -1
+    }
+
+    , mounted() {
+        this.connect()
+    }
+
+    , methods: {
+
+        connect() {
+            let p = this.address
+            let ws = new WebSocket(p);
+            ws.onmessage = this.socketMessage;
+            ws.onopen = this.socketOpen;
+            this.webSocket = ws;
+        }
+
+        , socketMessage(d){
+            this.$emit('websocketData', d)
+            //console.log(d);
+            this.socketMessages.push({
+                type: 'in'
+                , data: d.data
+            })
+        }
+
+        , socketOpen(d){
+            console.log('open', d);
+            this.connected = true
+        }
+
+        , sendMessage(){
+            this.webSocket.send(this.message)
+            this.socketMessages.push({
+                type: 'out'
+                , data: this.message
+            })
+            this.message = ''
+        }
+
+
+        , fetch(event, partial){
+            let path = partial == undefined ? this.$refs.address.value: partial;
+            console.log('path', path)
+            let fullpath = `${this.basePath}${path}`
+            $.get(fullpath, function(data){
+                this.renderPath(path, data)
+            }.bind(this))
+        }
+
+        , renderPath(path, data) {
+            console.log('got', data)
+            cleanData.push({path, data})
+            let dataCopy = JSON.parse(JSON.stringify(data))
+            this.requests.push({ path, dataCopy })
+
+        }
+    }
+})
