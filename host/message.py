a22b86ba9f6e5143413e00eda2459109e6c65fe5
Jay Jagpal <jay@strangemother.com>
2018-03-22 21:40:23 +0000

    MetaMessage, A message class with meta data
    A new class to replicate the internal message and allow decorating the
    message with additional data of which is parsed as a final `render`.
    
    Also compatible with string spaces.

--- host/message.py
+++ host/message.py
@@ -1,10 +1,143 @@
 """Message management and parsing
 """
 
-
 # Incoming chunked byte data is held in chunks for each client
 # until a message is complete.
 handling_stacks = {}
+
+
+class MetaMessage(object):
+    '''A class to hold an inbound data value (from a socket) for internal
+    managment and session editing. Created in `received_message` given a
+    Text or Binary message from the socket, and the attached client.
+
+    A result content to push back to clients is altered by the session and
+    any client translators.
+    '''
+    def __init__(self, message, client=None):
+        self.message = message
+        self.client = client
+        self._decoded = None
+        self.content = ()
+        self.content_keys = {'client_id', 'value'}
+
+    def copy(self, other=None):
+        other = other or self
+        """Perform a shallow copy of the current message"""
+        msg = MetaMessage(other.message, other.client)
+        msg._decoded = other._decoded
+        # shallow copy the tuple
+        msg.content = other.content + ()
+        msg.content_keys = other.content_keys.copy()
+        return msg
+
+    @property
+    def is_binary(self):
+        return self.message.is_binary
+
+    @property
+    def is_text(self):
+        return self.message.is_text
+
+    @property
+    def data(self):
+        return self.message.data
+
+    @property
+    def completed(self):
+        return self.message.completed
+
+    @property
+    def encoding(self):
+        return self.message.encoding
+
+    def __len__(self):
+        return len(self.message.__unicode__())
+
+    def __str__(self):
+        return self.message.__str__()
+
+    def __unicode__(self):
+        return self.message.__unicode__()
+
+    def utf8_decode(self, bytes_str=None):
+        data = bytes_str or self.message.data
+        return data.decode('utf-8')
+
+    def render(self):
+        '''return a complete version of the internal message for
+        transport to a client. The internal session, client and translators
+        are used for a final string or binary.
+        '''
+        decoded = self.decode_complete()
+
+        if decoded is None:
+            print('Message is None or not complete.')
+            return None
+
+        client = self.client
+        result = (
+            # The value or message content as default
+            ('value', decoded, ),
+            # The identity of the original client
+            ('client_id', client.id if hasattr(client, 'id') else id(client), ),
+            )
+
+        # Map in the extra data assigned by plugins.
+        # The ordered set is handled later with a translator
+        for item in self.content:
+            print('Processing keys {}'.format(item))
+            if item[0] in self.content_keys:
+                result += ( item, )
+            else:
+                print('Message rendering does not have content "{}"'.format(item))
+        return result
+
+    def decode_complete(self, cache=True):
+        '''
+        perform a utf8_decode if the internal message is `completed`.
+        Return None if the message is not complete.
+        '''
+
+        # Detect switch,
+        # encat or continue
+        data = self.message.data
+        client = self.client
+        text = None
+        message = self.message
+
+        if self._decoded is not None and cache is True:
+            return self._decoded
+
+        if self.completed:
+
+            if client.id in handling_stacks:
+                # If previous bytes exist, stack the last message then create
+                # a final byte string.
+                handling_stacks[client.id] += (self.data)
+                final = bytes()
+                for str_bytes in handling_stacks[client.id]:
+                    final += str_bytes
+                # remove the old stack data.
+                del handling_stacks[client.id]
+                # convert bytes to readable
+                text = self.utf8_decode(final)
+            else:
+                text = self.utf8_decode()
+        else:
+            if client.id not in handling_stacks:
+                handling_stacks[client.id] = ()
+
+            handling_stacks[client.id] += (self.data, )
+
+        if text is None:
+            return
+
+        if cache is True:
+            self._decoded = text
+
+        return text
+
 
 
 def postmaster(message, client, clients):
@@ -15,7 +148,7 @@ def postmaster(message, client, clients):
     if message.is_binary:
         return handle_binary(message, client, clients)
 
-    return handle_text(message, client, clients)
+    return message.handle_text(clients)
 
 
 def handle_text(message, client, clients):
@@ -49,9 +182,12 @@ def handle_text(message, client, clients):
 
         handling_stacks[client.id] += (message.data, )
 
-
     if text is None:
         return
+
+    if isinstance(message, MetaMessage):
+        message._decoded = text
+        return message
 
     return text
 
@@ -83,5 +219,5 @@ def broadcast(data, client, clients, is_binary=False, ignore=None, cid=None):
     for name in clients:
         if clients[name] in ignore:
             continue
-        data = client.session.encode(data, client, clients, is_binary)
-        clients[name].send(data, is_binary)
+        res = client.session.encode(data, client, clients, is_binary)
+        clients[name].send(res, is_binary)
