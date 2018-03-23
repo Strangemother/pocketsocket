56460a4d7c273d8f3b9568be29e01a3c9d6333c1
Jay Jagpal <jay@strangemother.com>
2018-03-23 19:24:09 +0000

    Capture browser requests

--- host/main.py
+++ host/main.py
@@ -46,10 +46,37 @@ class SessionClient(WebSocket):
         self.session.remove(self)
 
 
+base_html = b'''<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Content</title>
+</head>
+<body>
+    <p>Hey there short stuff.</p>
+</body>
+</html>
+'''
+
+
+class SessionServer(WebSocketWSGIApplication):
+    def __call__(self, environ, start_response):
+        if environ.get('REQUEST_METHOD') != 'GET':
+            raise HandshakeError('HTTP method must be a GET')
+
+        ws_token = environ.get("HTTP_UPGRADE", None)
+        if ws_token is None:
+            if environ['SERVER_PROTOCOL'] == 'HTTP/1.1':
+                start_response("200 OK", [])
+                return [base_html]
+        # Test for Mapping offload
+        return super(SessionServer, self).__call__(environ, start_response)
+        #
+
 def main(address=None):
     address = address or ('localhost', 8009,)
     log('Run', address)
-    server = WSGIServer(address, WebSocketWSGIApplication(handler_cls=SessionClient))
+    server = WSGIServer(address, SessionServer(handler_cls=SessionClient))
     #_man.start()
     #_man.run()
     setup_session(address, server)
